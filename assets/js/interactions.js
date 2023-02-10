bb43d9b6602d8ad72360af48a92fed1e2ec4093f
elaine2700 <easerrano.e@gmail.com>
2023-02-09 20:49:01 -0800

    Added responsive Nav Menu

---
+++ assets/js/interactions.js
@@ -0,0 +1,20 @@
+
+// menu Button
+let toogleMenuButton = document.querySelector('.toggle');
+
+function toggleMenu() {
+    let x = document.querySelector(".main-nav");
+    if(x.className == "main-nav")
+        x.className += " display";
+    else
+        x.className = "main-nav";
+
+    //todo change icon from bars to x
+    let icon = x.querySelector(".icon");
+    if(icon.className === "icon fa fa-bars")
+        icon.className = "icon fa-solid fa-x ";
+    else
+        icon.className = "icon fa fa-bars";
+}
+
+toogleMenuButton.onclick = toggleMenu;
