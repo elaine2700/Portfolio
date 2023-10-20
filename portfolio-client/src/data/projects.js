e8eefc09091f33261cba8f88d39d71a483d8d0a2
elaine2700 <easerrano.e@gmail.com>
2023-10-19 19:22:22 -0700

    Updated projects information

---
+++ portfolio-client/src/data/projects.js
@@ -0,0 +1,111 @@
+const projects = [
+    {
+        id: 0,
+        title: "VR Arki Academy",
+        image: "/src/assets/images/ArkiAcademy_Banner.jpg",
+        projectType: "VR/AR",
+        tools: ["Unity"],
+        programmingLanguages: ["C#"],
+        description: "A building system where the user can design and create their dream house in Virtual Reality.",
+        linkCode: "https://github.com/elaine2700/VR_ArchiAcademy",
+        live: null,
+        trailer: "https://youtu.be/S3NLhjTkV9Q",
+        content: ".txt"
+    },
+    {
+        id: 1,
+        title: "Hexagonal Procedural World",
+        image: "/src/assets/images/Procedural_World.png",
+        projectType: "Unity Tool",
+        tools: ["Unity"],
+        programmingLanguages: ["C#"],
+        description: "A tool to create a Procedural world with hexagons.",
+        linkCode: "https://github.com/elaine2700/Hexagonal_World",
+        live: null,
+        trailer: "https://youtu.be/US76BYYP_ZM",
+        content: ".txt"
+    },
+    {
+        id: 2,
+        title: "Model Viewer",
+        image: "/src/assets/images/Bike_Viewer.png",
+        projectType: "Web",
+        tools: ["Unity"],
+        programmingLanguages: ["C#"],
+        description: "A tool to display the breakdown of an specific 3d model",
+        linkCode: "https://github.com/elaine2700/3d-Model-Viewer",
+        live: null,
+        trailer: "https://youtu.be/US76BYYP_ZM",
+        content: ".txt"
+    },
+    {
+        id: 3,
+        title: "To do list App",
+        image: "/src/assets/images/ToDo-List_thumbnail.png",
+        projectType: "Web",
+        tools: ["React"],
+        programmingLanguages: ["HTML", "CSS", "JS"],
+        description: "A simple tool to make a list of tasks.",
+        linkCode: "https://github.com/elaine2700/Hexagonal_World",
+        live: "https://portfolio.elaineserrano.com/to-do-list",
+        trailer: "https://youtu.be/US76BYYP_ZM",
+        content: ".txt"
+    },
+    {
+        id: 4,
+        title: "Visions of Yesterday",
+        image: "/src/assets/images/VOY_banner.jpg",
+        projectType: "VR/AR",
+        tools: ["Unity"],
+        programmingLanguages: ["C#"],
+        description: "A Virtual Reality narrative where the user will experience dementia symptoms.",
+        linkCode: "https://github.com/JorgeLeonS/DementiaProject",
+        live: "https://portfolio.elaineserrano.com/to-do-list",
+        trailer: "",
+        content: ".txt"
+    },
+    {
+        id: 5,
+        title: "Movie Library",
+        image: "/src/assets/images/movielibrary_thumbnail.png",
+        projectType: "Web",
+        tools: ["React"],
+        programmingLanguages: ["HTML", "CSS", "JS"],
+        description: "Type the name of a movie and get the movie information",
+        linkCode: "https://github.com/elaine2700/movielibrary",
+        live: "https://portfolio.elaineserrano.com/movielibrary",
+        trailer: "",
+        content: ".txt"
+    },
+    {
+        id: 6,
+        title: "Rube Goldberg Machine",
+        image: "/src/assets/images/RubeGoldbergMachine_Banner.jpg",
+        projectType: "VR/AR",
+        tools: ["Unreal", "Blueprints"],
+        programmingLanguages: [],
+        description: "A machine that completes a single task in a complicated way. The task for my machine is to open a box full of cookies.",
+        linkCode: "",
+        live: "",
+        trailer: "",
+        content: ".txt"
+    },
+    {
+        id: 7,
+        title: "Artist Mode On Website",
+        image: "/src/assets/images/artistmodeon_thumbnail.png",
+        projectType: "Web",
+        tools: [],
+        programmingLanguages: ["HTML", "CSS"],
+        description: "Website Design",
+        linkCode: "https://www.artistmodeon.com/",
+        live: "https://github.com/elaine2700/ArtistModeOn_website",
+        trailer: "",
+        content: ".txt"
+    },
+    
+
+]
+export default projects
+
+    
