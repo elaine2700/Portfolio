e1cbddcf572dbfed5a0c0c8bc0219369d409fc40
elaine2700 <easerrano.e@gmail.com>
2023-10-19 18:35:47 -0700

    Projects View

--- portfolio-client/src/App.jsx
+++ portfolio-client/src/App.jsx
@@ -1,34 +1,73 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import ProjectCard from './components/ProjectCard'
+import projects from './data/projects.js'
+import './assets/app.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const projectsData = projects;
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="grid">
+            <header className="grid-box" id="personal">
+                <div>
+                    <div className="profile-card window">
+                        <div className="profile-image">
+                            <img id="icon-base" src="images/Icon_Base.png"/>
+                            <img id="icon-lens" className="invisible transition-right" src="images/Icon_HMD.png"/>
+                            <div id="VR-checkbox">
+                                {
+                                  //<span>VR</span>
+                                  //<input id="VR-Toggle" type="checkbox"/>
+                                  //<label className="switch" for="VR-Toggle"></label>
+                                }
+                                
+                            </div>
+                        </div>
+                        <div className="name">
+                            <h1>Elaine Serrano</h1>
+                            <h2>Developer & Designer</h2>
+                        </div> 
+                    </div>  
+                    <nav className="main-nav">
+                        <ul>
+                            <li className="toggle"><a className="clickable"><i className="icon fa fa-bars"></i></a></li>
+                            <li><a className="clickable" href="index.html">Home</a></li>
+                            <li><a className="clickable current-selection" href="projects.html">Projects</a></li>
+                            <li className="hide"><a className="clickable" href="resume.html">Resume</a></li>
+                        </ul>
+                    </nav> 
+                </div>      
+            </header>
+
+            <main className="grid-box" id="content">
+                <section>
+                    <header>
+                        <h1 className="section-title">Projects</h1>
+                    </header>
+                </section>
+                {
+                  projectsData.map((project)=>(
+                    <ProjectCard key={project.id} project={project}/>
+                  ))
+                }
+
+            </main>
+
+            <footer className="grid-box" id="contact">
+                <div className="window">
+                    <ul className="social-media">
+                        <li><a href="https://www.linkedin.com/in/elaine-serrano-echeverria/" className="fa-brands fa-linkedin"></a></li>
+                        <li><a href="https://github.com/elaine2700" className="fa-brands fa-github"></a></li>
+                    </ul>
+                    <ul className="page-info">
+                        <li>&copy;2023 Elaine Serrano</li>
+                        <li>Contact me at <span className="bold"><a href="mailto:contact@elaineserrano.com">contact@elaineserrano.com</a></span></li>
+                        <li>Last Updated on February, 2023</li>
+                        <li>Designed & coded from Scratch</li>
+                    </ul>
+                </div>    
+            </footer>
+        </div>
   )
 }
 
---
+++ portfolio-client/src/components/ProjectCard.jsx
@@ -0,0 +1,29 @@
+
+const ProjectCard = ({project}) => {
+    return (
+        <div className="even-columns fit">
+            <div className="window">
+                <h4 className="window-title">{project.title}</h4>
+                <div className="thumbnail-fitter">
+                    <img className="bordered" src={project.image}/>  
+                    <ul className="tags bordered">
+                        <li className="tag">{project.projectType}</li>
+                        {
+                            project.tools.map((tag, index)=>(
+                                <li key={index} className="tag">{tag}</li>
+                            ))
+                        }
+                        {
+                            project.programmingLanguages.map((tag, index)=>(
+                                <li key={index} className="tag">{tag}</li>
+                            ))
+                        }
+                    </ul>
+                    <button className="centered clickable" >More</button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ProjectCard
