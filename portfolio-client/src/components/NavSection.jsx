bda7d29655f279b2c2c64df600e4ab110646f2f0
elaine2700 <easerrano.e@gmail.com>
2023-10-26 14:13:15 -0700

    Fixed Responsive adaptation

--- portfolio-client/src/components/NavSection.jsx
+++ portfolio-client/src/components/NavSection.jsx
@@ -1,24 +1,50 @@
-import ProfileCard from './ProfileCard'
-import { Link } from 'react-router-dom'
+import ProfileCard from './ProfileCard';
+import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import {List, X} from 'react-bootstrap-icons'
 
 const NavSection = () => {
+
+  const [popMenu, setPopMenu] = useState(true);
+  const [navBarClass, setNavBarClass] = useState('main-nav');
+  const [toggleClass, setToggleClas] = useState('toggle');
+
+  const toggleMenu = () => {
+    setPopMenu(!popMenu);
+    if(popMenu){
+      setNavBarClass('main-nav display window')
+      setToggleClas('toggle clickable outline')
+    }  
+    else {
+      setNavBarClass('main-nav')
+      setToggleClas('toggle clickable')
+    }
+      
+      
+  };
+
   return (
     <header id="personal" className='window shadow'>
           <div>
             <ProfileCard/>
-
-            <nav className="main-nav">
+            <nav className={navBarClass} >
+              
               <ul>
-                <li className="toggle">
-                  <a className="clickable"><i className="icon fa fa-bars"></i></a></li>
-                <li>
-                  <Link to='/' className='clickable outline'>Home</Link>
-                  </li>
-                <li>
-                  <Link to='/projects' className='clickable outline'>Projects</Link>
+                <li className={toggleClass}>
+                <a  onClick={toggleMenu}>
+                {
+                  popMenu ? <List/> : <X/>
+                }
+              </a>
                 </li>
                 <li>
-                  <Link to='/resume' className='clickable outline'>Resume</Link>
+                  <Link to='/' className='clickable outline' onClick={toggleMenu}>Home</Link>
+                  </li>
+                <li>
+                  <Link to='/projects' className='clickable outline' onClick={toggleMenu}>Projects</Link>
+                </li>
+                <li>
+                  <Link to='/resume' className='clickable outline' onClick={toggleMenu}>Resume</Link>
                   </li>
               </ul>
             </nav>
