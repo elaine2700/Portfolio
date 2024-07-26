3b737a5e0d5e02fdfd2333d2606bffebd5a4222c
elaine2700 <103074370+elaine2700@users.noreply.github.com>
2024-07-26 13:24:24 -0600

    Read Json Files in templates

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,13 @@
+
 module.exports = function(eleventyConfig){
     eleventyConfig.addPassthroughCopy("src/assets");
+    
     return {
         dir:{
             input: 'src',
             includes: '_includes',
             output: '_site',
+            data: "_data"
         },
         templateFormats: ['md', 'njk', 'html'],
         markdownTemplateEngine: 'njk',
