4a3992a886ffa4d09fa5657e31fcb7a70cc68e55
elaine2700 <103074370+elaine2700@users.noreply.github.com>
2024-07-29 17:18:41 -0600

    Add hidden class on click button

--- src/assets/js/projects.js
+++ src/assets/js/projects.js
@@ -1,15 +1,32 @@
 document.addEventListener("DOMContentLoaded", function(){
-    const filterBar = document.getElementById("filterBar");
+    
     const projects = document.querySelectorAll(".d-project");
     // For each button in filter bar, use filter word to select through category
     // for all project cards.
-    const filterWord = "VR";
-    for (const project of projects) {
-        const projectCategory = project.querySelector(".d-projectType");
-        console.log(projectCategory);
-    }
+    
 
     // add event to buttons
+    const filterBar = document.getElementById("filterBar");
     const buttons = filterBar.querySelectorAll(".button");
-    
+    for (const button of buttons) {
+        const filterLabel = button.innerHTML;
+        button.addEventListener("click", function(){
+            console.log(`${filterLabel} button`);
+            filterProjects(filterLabel, projects);
+        })
+    }
 })
+
+function filterProjects(filterKey, projects){
+    if(filterKey === "All"){
+        console.log("reset");
+        return;
+    }
+    for (const project of projects) {
+        const projectCategory = project.querySelector(".d-projectType");
+        if(filterKey !== projectCategory.innerHTML){
+            project.classList.add("hidden");
+        }
+        console.log(projectCategory);
+    }
+}
